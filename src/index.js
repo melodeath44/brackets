ca808ecdcc9430731b2aef1b6407ce38f27182db
Vitaly <melodeath44@mail.ru>
2023-08-27 04:41:23 +0300

    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const array = str.split(',');
+  const arrayReverse = array.reverse()
+  let x
+  let y
+for(let i = 0; i < array.length; i++){
+   x = array[i]
+   
+}
+for(let o = 0; o < arrayReverse.length; o++){
+   y = arrayReverse[o]
+}
+if(x === y){
+  return true
+}
+ else return false
+
+
+
 }
